84a9576b1220ab6e08365fe2b79b9ec3cf74d4c1
Roman Valls Guimera <brainstorm@nopcode.org>
2018-06-29 16:10:46 +1000

    We need to tell the ecs-agent which cluster to run this AMI in, provision volumes, etc.. As seen in https://aws.amazon.com/blogs/compute/building-high-throughput-genomic-batch-workflows-on-aws-batch-layer-part-3-of-4/ but different [ci skip]

---
+++ boostrap_instance.sh
@@ -0,0 +1,23 @@
+#!/bin/sh -x
+# credit where is due: https://aws.amazon.com/blogs/compute/building-high-throughput-genomic-batch-workflows-on-aws-batch-layer-part-3-of-4/
+
+AWS_DEV="/dev/xvdb"
+AWS_AZ="ap-southeast-2c"
+AWS_INSTANCE=`curl https://169.254.169.254/latest/meta-data/instance-id`
+
+# All root operations
+sudo su
+
+# XXX: AZs on volumes, try to stick to one or make sure instances and volumes are under the same?
+VOL_ID=aws ec2 create-volume --availability-zone "$AWS_AZ" --encrypted --size 500 --volume-type st1 --tag-specifications "awsbatch" | jq .VolumeId
+aws ec2 attach-volume --device "$AWS_DEV" --instance-id "$AWS_INSTANCE" --volume-id "$VOL_ID"
+
+# Format/mount
+mkfs -t btrfs "$AWS_DEV"
+echo -e "$AWS_DEV\t/mnt\tbtrfs\tdefaults\t0\t0" | tee -a /etc/fstab
+mount –a
+
+# XXX: Why not leave the agent running?
+# XXX: Need to gather/inject ECS cluster id here anyways
+sudo stop ecs
+sudo rm -rf /var/lib/ecs/data/ecs_agent_data.json
