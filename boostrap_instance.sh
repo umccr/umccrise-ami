c3ba30ce1c6f66e46d915219301ff03451213ea5
Roman Valls Guimera <brainstorm@nopcode.org>
2018-07-02 12:28:03 +1000

    Userdata build, fix for SSH public keys

--- boostrap_instance.sh
+++ boostrap_instance.sh
@@ -2,13 +2,13 @@
 # credit where is due: https://aws.amazon.com/blogs/compute/building-high-throughput-genomic-batch-workflows-on-aws-batch-layer-part-3-of-4/
 
 AWS_DEV="/dev/xvdb"
+# XXX: AZs on volumes, try to stick to one or make sure instances and volumes are under the same?
 AWS_AZ="ap-southeast-2c"
 AWS_INSTANCE=`curl https://169.254.169.254/latest/meta-data/instance-id`
 
 # All root operations
 sudo su
 
-# XXX: AZs on volumes, try to stick to one or make sure instances and volumes are under the same?
 VOL_ID=aws ec2 create-volume --availability-zone "$AWS_AZ" --encrypted --size 500 --volume-type st1 --tag-specifications "awsbatch" | jq .VolumeId
 aws ec2 attach-volume --device "$AWS_DEV" --instance-id "$AWS_INSTANCE" --volume-id "$VOL_ID"
 
@@ -19,5 +19,8 @@ mount –a
 
 # XXX: Why not leave the agent running?
 # XXX: Need to gather/inject ECS cluster id here anyways
-sudo stop ecs
-sudo rm -rf /var/lib/ecs/data/ecs_agent_data.json
+#sudo stop ecs
+#sudo rm -rf /var/lib/ecs/data/ecs_agent_data.json
+
+# Pull in all reference data to /mnt
+aws s3 sync s3://umccr-primary-data-dev/ /mnt
