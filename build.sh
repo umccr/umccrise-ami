2403782c9be5ab4de37805bda153a55c791d4882
Roman Valls Guimera <brainstorm@nopcode.org>
2018-08-10 10:22:19 +1000

    Adopt @freisinger new vault secret injection mechanism for TravisCI builds, build new UMCCRISE docker image on AMI

--- build.sh
+++ build.sh
@@ -2,15 +2,10 @@
 set -e
 set -o pipefail
 
+temp_role=$(vault write aws/sts/ops_admin_no_mfa ttl=15m -format=json)
 
-# assume the ops_admin_no_mfa role to get the needed AWS permissions and set the stage for the following packer build
+# assume the packer_role role to get the needed AWS permissions and set the stage for the following packer build
 export AWS_REGION=ap-southeast-2
-
-temp_role=$(aws sts assume-role --role-arn "arn:aws:iam::620123204273:role/packer_role" --role-session-name "temp_session")
-
-export AWS_ACCESS_KEY_ID=$(echo $temp_role | jq .Credentials.AccessKeyId | xargs)
-export AWS_SECRET_ACCESS_KEY=$(echo $temp_role | jq .Credentials.SecretAccessKey | xargs)
-export AWS_SESSION_TOKEN=$(echo $temp_role | jq .Credentials.SessionToken | xargs)
-
-# with the new access credentials packer can now build the AMI
-packer build -machine-readable packer.json | sudo tee packer-build.log
+export AWS_ACCESS_KEY_ID=$(echo $temp_role | jq '.data | .access_key' | xargs)
+export AWS_SECRET_ACCESS_KEY=$(echo $temp_role | jq '.data | .secret_key' | xargs)
+export AWS_SESSION_TOKEN=$(echo $temp_role | jq '.data | .security_token' | xargs)
--- send-datadog-event.sh
+++ send-datadog-event.sh
@@ -1,20 +1,23 @@
 #!/bin/bash -e
+set -e
+set -o pipefail
 
-APP="umccrise"
+APP=$1
+TAGS=$2
 
-# install DataDog Python library
-pip install datadog
+DATADOG_API_KEY=$(vault kv get -format=json kv/datadog | jq -r '.data.api_key')
 
-# Set up
-tee ~/.dogrc << END
-[Connection]
-apikey = ${DATADOG_API_KEY}
-appkey = ${DATADOG_APP_KEY}
-END
+event_text="%%% \n $TRAVIS_REPO_SLUG: new build (no [$TRAVIS_BUILD_NUMBER](https://travis-ci.org/umccr/test-vault-secrets-injection/builds/$TRAVIS_BUILD_ID)) on branch $TRAVIS_BRANCH succeeded for commit [${TRAVIS_COMMIT:0:12}](https://github.com/umccr/test-vault-secrets-injection/commit/${TRAVIS_COMMIT}) \n %%%"
 
-echo "Extracting AMI ID"
-ami_id=`grep 'artifact,0,id' packer-build.log | cut -d, -f6 | cut -d: -f2`
+echo "Generating DataDog event"
+curl -X POST -H "Content-type: application/json" \
+-d "{
+      \"title\": \"New $APP event created\",
+      \"text\": \"$event_text\",
+      \"priority\": \"normal\",
+      \"tags\": [\"$TAGS\"],
+      \"alert_type\": \"info\"
+}" \
+"https://api.datadoghq.com/api/v1/events?api_key=$DATADOG_API_KEY"
 
-echo "Extracted AMI ID: $ami_id. Sending DataDog event..."
-dog event post --no_host --tags aws,ami,$APP --type travis "New AMI created" "$ami_id build from commit $TRAVIS_COMMIT"
 echo "Event successfully sent."
