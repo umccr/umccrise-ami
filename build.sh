1877b30e404f63b154a0656e56270d88780039bb
Roman Valls Guimera <brainstorm@nopcode.org>
2018-08-10 10:46:36 +1000

    Actually build the AMI with packer

--- build.sh
+++ build.sh
@@ -9,3 +9,6 @@ export AWS_REGION=ap-southeast-2
 export AWS_ACCESS_KEY_ID=$(echo $temp_role | jq '.data | .access_key' | xargs)
 export AWS_SECRET_ACCESS_KEY=$(echo $temp_role | jq '.data | .secret_key' | xargs)
 export AWS_SESSION_TOKEN=$(echo $temp_role | jq '.data | .security_token' | xargs)
+
+# with the new access credentials packer can now build the AMI
+packer build -machine-readable packer.json | sudo tee packer-build.log
